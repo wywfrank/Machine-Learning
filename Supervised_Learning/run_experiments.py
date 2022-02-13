b8eb547e1fbe00b15182d816f2632bbf1df5ac43
Frank Wan <frank.wan@intervarsity.org>
2022-02-12 19:35:18 -0500

    commenting and naming updates

--- Supervised_Learning/run_experiments.py
+++ Supervised_Learning/run_experiments.py
@@ -1,27 +1,29 @@
 # Script to run experiments
 
+from pickle import TRUE
+from re import I
+from tracemalloc import stop
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 
 from adaboost import AdaBoost
 from decision_trees import DecisionTree
 from k_nearest_neighbors import KNN
 from neural_networks import NeuralNetwork
+from gradient_boost import GradientBoost
 from support_vector_machines import SVM
-
-from sklearn.datasets import load_breast_cancer, fetch_openml
-from sklearn.model_selection import train_test_split
 
 IMAGE_DIR = 'images/'
 
 
-def load_dataset(dataset='WDBC', split_percentage=0.2, visualize=False):
+def load_dataset(split_percentage=0.2, visualize=False):
     """Load MNIST or WDBC.
 
        Args:
-           dataset (string): dataset, WDBC or MNIST.
            split_percentage (float): validation split.
            visualize (bool): True if some of the dataset images have to been shown.
 
@@ -32,80 +34,55 @@ def load_dataset(dataset='WDBC', split_percentage=0.2, visualize=False):
            y_test (ndarray): test labels.
        """
 
-    datasets = ['WDBC', 'MNIST']  # datasets names
-    print('Loading {} Dataset'.format(dataset))
-
-    if dataset == datasets[0]:
-
         # Load WDBC
-        data = load_breast_cancer()
-        x, y, labels, features = data.data, data.target, data.target_names, data.feature_names
+    data = load_breast_cancer()
+    x, y, labels, features = data.data, data.target, data.target_names, data.feature_names
+   
 
-        # If some of the dataset images have to been shown
-        if visualize:
+    if visualize:
 
-            # Build dataset and assign labels
-            df = pd.DataFrame(x, columns=features)
-            df['labels'] = y
-            df['labels'] = df['labels'].map({1: 'B', 0: 'M'})
+        # Build dataset and assign labels
+        df = pd.DataFrame(x, columns=features)
+        df['labels'] = y
+        df['labels'] = df['labels'].map({1: 'B', 0: 'M'})
 
-            # Plot instances distribution
-            plt.figure()
-            sns.set(style='darkgrid')
-            sns.countplot(x='labels', data=df, palette={'B': 'b', 'M': 'r'})
-            plt.title('{} Instances Distribution'.format(dataset))
-            plt.savefig(IMAGE_DIR + '{}_Instances_Distribution'.format(dataset))
+        # Plot instances distribution
+        plt.figure()
+        sns.set(style='darkgrid')
 
-            # Plot heatmap of correlations
-            plt.figure(figsize=(15, 15))
-            sns.heatmap(df.corr(), annot=True, square=True, cmap='coolwarm')
-            plt.savefig(IMAGE_DIR + '{}_Features_Correlation'.format(dataset))
+        sns.countplot(x='labels', data=df, palette={'B': 'b', 'M': 'r'})
+        plt.title('WDBC Instances Distribution')
+        plt.savefig(IMAGE_DIR + 'WDBC_Instances_Distribution')
 
-            # Plot scatter matrix of features
-            plt.figure(figsize=(15, 15))
-            sns.pairplot(df, hue='labels', palette={'B': 'b', 'M': 'r'})
-            plt.savefig(IMAGE_DIR + '{}_Scatter_Matrix_of_Features'.format(dataset))
+        # Plot heatmap of correlations
+        plt.figure(figsize=(15, 15))
+        sns.heatmap(df.corr(), annot=True, square=True, cmap='coolwarm')
+        plt.savefig(IMAGE_DIR + 'WDBC_Features_Correlation')
 
-            # Plot features distributions
-            bins = 12
-            plt.figure(figsize=(15, 15))
-            for i, feature in enumerate(features):
-                plt.subplot(5, 2, i + 1)
+        # Plot scatter matrix of features
+        plt.figure(figsize=(15, 15))
+        sns.pairplot(df, hue='labels', palette={'B': 'b', 'M': 'r'})
+        plt.savefig(IMAGE_DIR + 'WDBC_Scatter_Matrix_of_Features')
 
-                sns.displot(x[y == 0, i], bins=bins, color='red', label='M')
-                sns.displot(x[y == 1, i], bins=bins, color='blue', label='B')
+        print("Number of features: ", len(features))
+        
+        Plot features distributions
+        plt.figure(figsize=(15, 15))
 
-                plt.legend(loc='lower left')
-                plt.xlabel(feature)
+        for i, feature in enumerate(features):
+            plt.subplot(15, 2, i + 1)
 
-            plt.tight_layout()
-            plt.savefig(IMAGE_DIR + '{}_Features_Discrimination'.format(dataset))
-            plt.close(fig='all')
+            sns.distplot(x[y == 0, i], color='red', label='M')
+            sns.distplot(x[y == 1, i], color='blue', label='B')
 
-    elif dataset == datasets[1]:
+            plt.legend(loc='upper left')
+            plt.xlabel(feature)
 
-        # Load original MNIST
-        data = fetch_openml('mnist_784')
-        x, y = data.data, data.target
-
-        # Generate a statified smaller subset of MNIST
-        x, _, y, _ = train_test_split(x, y, test_size=0.98, shuffle=True, random_state=42, stratify=y)
-        labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  # labels
-
-        if visualize:
-            # Show images with labels
-            for i in range(2 * 3):
-                plt.subplot(2, 3, i + 1)
-                plt.imshow(x[i].reshape((28, 28)), cmap=plt.cm.gray)
-                plt.title(y[i], size=12)
-                plt.xticks(())
-                plt.yticks(())
-
-            plt.show()
-
-    else:
-        # Else dataset not available
-        raise Exception('Wrong dataset name. Datasets available = {}'.format(datasets))
+        # plt.tight_layout()
+        plt.show()
+        plt.savefig(IMAGE_DIR + 'WDBC_Features_Discrimination')
+        plt.close(fig='all')
+     
 
     # Split dataset in training and validation sets, preserving classes representation
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=split_percentage, shuffle=True,
@@ -149,58 +126,69 @@ def experiment(x_train, x_test, y_train, y_test):
                    train_sizes=training_sizes)
 
     # # Support Vector Machines
-    # print('\n--------------------------')
-    # svm = SVM(c=1., kernel='rbf', degree=3, gamma=0.001, random_state=42)
-    # svm.experiment(x_train, x_test, y_train, y_test,
-    #                cv=10,
-    #                y_lim=0.2,
-    #                C_range=[1, 5] + list(range(10, 100, 20)) + list(range(100, 1000, 50)),
-    #                kernels=['linear', 'poly', 'rbf'],
-    #                gamma_range=np.logspace(-7, 0, 50),
-    #                poly_degrees=[2, 3, 4],
-    #                train_sizes=training_sizes)
+    print('\n--------------------------')
+    svm = SVM(c=5., kernel='rbf', degree=3, gamma=0.001, random_state=42)
+    svm.experiment(x_train, x_test, y_train, y_test,
+                   cv=10,
+                   y_lim=0.6,
+                   C_range=[1, 5] + list(range(10, 100, 20)) + list(range(100, 1000, 50)),
+                #    kernels=['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+                   kernels=['rbf'],
+                   gamma_range=np.logspace(-7, 0, 50),
+                   poly_degrees=[2, 3, 4],
+                   train_sizes=training_sizes
+                   )
 
-    # # Decision Trees
-    # print('\n--------------------------')
-    # dt = DecisionTree(max_depth=1, min_samples_leaf=1, random_state=42)
-    # dt.experiment(x_train, x_test, y_train, y_test,
-    #               cv=10,
-    #               y_lim=0.1,
-    #               max_depth_range=list(range(1, 50)),
-    #               min_samples_leaf_range=list(range(1, 30)),
-    #               train_sizes=training_sizes)
+    # Pruned Decision Trees
+    print('\n--------------------------')
+    dt = DecisionTree(max_depth=10, min_samples_leaf=1, ccp_alpha=0.0)
+    dt.experiment(x_train, x_test, y_train, y_test,
+                  cv=10,
+                  y_lim=0.1,
+                  max_depth_range=list(range(1, 50)),
+                  min_samples_leaf_range=list(range(1, 30)),
+                  ccp_alpha_range=np.linspace(0,1,51),
+                  train_sizes=training_sizes)
 
-    # # AdaBoost
-    # print('\n--------------------------')
-    # boosted_dt = AdaBoost(n_estimators=50, learning_rate=1., max_depth=3, random_state=42)
-    # boosted_dt.experiment(x_train, x_test, y_train, y_test,
-    #                       cv=10,
-    #                       y_lim=0.2,
-    #                       max_depth_range=list(range(1, 30)),
-    #                       n_estimators_range=[1, 3, 5, 8] + list(range(10, 100, 5)) + list(range(100, 1000, 50)),
-    #                       learning_rate_range=np.logspace(-6, 1, 50),
-    #                       train_sizes=training_sizes)
+    # AdaBoost
+    print('\n--------------------------')
+    boosted_dt = AdaBoost(n_estimators=50, learning_rate=1., max_depth=3, random_state=42)
+    boosted_dt.experiment(x_train, x_test, y_train, y_test,
+                          cv=10,
+                          y_lim=0.2,
+                          max_depth_range=list(range(1, 30)),
+                          n_estimators_range=[1, 3, 5, 8] + list(range(10, 100, 5)) + list(range(100, 1000, 50)),
+                          learning_rate_range=np.logspace(-6, 1, 50),
+                          train_sizes=training_sizes)
 
-    # # Neural Networks
-    # print('\n--------------------------')
-    # nn = NeuralNetwork(alpha=0.01, layer1_nodes=50, layer2_nodes=30, learning_rate=0.001, max_iter=100)
-    # nn.experiment(x_train, x_test, y_train, y_test,
-    #               cv=10,
-    #               y_lim=0.1,
-    #               alpha_range=np.logspace(-5, 1, 30),
-    #               learning_rate_range=np.logspace(-4, 0, 50),
-    #               train_sizes=training_sizes)
+    # GradientBoost
+    print('\n--------------------------')
+    boosted_dt = GradientBoost(n_estimators=50, learning_rate=1., max_depth=10, random_state=42)
+    boosted_dt.experiment(x_train, x_test, y_train, y_test,
+                          cv=10,
+                          y_lim=0.2,
+                          max_depth_range=list(range(1, 30)),
+                          n_estimators_range=[1, 3, 5, 8] + list(range(10, 100, 5)) + list(range(100, 1000, 50)),
+                          learning_rate_range=np.logspace(-6, 1, 50),
+                          train_sizes=training_sizes)
+
+    # Neural Networks
+    print('\n--------------------------')
+    nn = NeuralNetwork(layer1_nodes=17,layer2_nodes=1, learning_rate=0.001, max_iter=100)
+    nn.experiment(x_train, x_test, y_train, y_test,
+                  cv=10,
+                  y_lim=0.1,
+                  first_hidden_layer_sizes_range=[(i) for i in range(1,30)],
+                  second_hidden_layer_sizes_range=[(17,i) for i in range(1,30)],
+                  learning_rate_range=np.logspace(-4, 0, 50),
+                  train_sizes=training_sizes)
 
 
 if __name__ == "__main__":
 
     # Run experiment 1 on WDBC
     print('\n--------------------------')
-    x_train, x_test, y_train, y_test = load_dataset(dataset='WDBC', visualize=False)
-    print(x_train)
+    x_train, x_test, y_train, y_test = load_dataset(visualize=False)
+
     experiment(x_train, x_test, y_train, y_test)
 
-
-    # Run experiment 2 on MNIST
-    # x_train, x_test, y_train, y_test = load_dataset(dataset='MNIST', visualize=False)
-    # experiment(x_train, x_test, y_train, y_test)
